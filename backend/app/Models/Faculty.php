ec13bcefa1d13016759446f57d8aeaafecb1d008
Emil Auer <e.auer@itkaufmann.at>
2023-12-09 14:31:45 +0100

    Implement faculties table and model; Add faculty_id to users table

---
+++ backend/app/Models/Faculty.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+class Faculty
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name'
+    ];
+}
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -20,7 +20,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $fillable = [
         'email',
         'password',
-        'role'
+        'role',
+
+        'faculty_id'
     ];
 
     /**
