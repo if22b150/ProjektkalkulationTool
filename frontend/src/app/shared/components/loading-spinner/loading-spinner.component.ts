d448c1d407aa440695db5ffdfd5390e1902661c2
Emil Auer <e.auer@itkaufmann.at>
2024-01-23 19:59:03 +0100

    Fix spinner component

--- frontend/src/app/shared/components/loading-spinner/loading-spinner.component.ts
+++ frontend/src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -6,8 +6,7 @@ import { Component } from '@angular/core';
     <div class="flex justify-content-center">
       <p-progressSpinner aria-label="Loading"></p-progressSpinner>
     </div>
-  `,
-  styleUrls: ['./loading-spinner.component.scss']
+  `
 })
 export class LoadingSpinnerComponent {
 
